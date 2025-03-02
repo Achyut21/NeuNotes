bd91101d87676998cb49b6f9a81de26d540558fd
Achyut Katiyar <achyutkatiyar@Achyuts-MacBook-Pro.local>
2025-03-02 07:31:53 -0500

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,49 @@
 // src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { useEffect } from "react";
+import useAuthStore from "./contexts/authStore";
 
 const App = () => {
+  const { user } = useAuthStore();
+
+  // Apply different background colors to different pages
+  useEffect(() => {
+    if (window.location.pathname === '/login') {
+      document.body.classList.add('login-page');
+    } else {
+      document.body.classList.remove('login-page');
+    }
+  }, []);
+
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <div className="flex flex-col min-h-screen">
+        {/* Only show navbar if not on login page */}
+        {window.location.pathname !== '/login' && <Navbar />}
+        
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <ProtectedRoute>
+                  <Home />
+                </ProtectedRoute>
+              }
+            />
+            {/* Add other routes as needed */}
+          </Routes>
+        </main>
+        
+        {/* Only show footer if not on login page */}
+        {window.location.pathname !== '/login' && <Footer />}
+      </div>
     </Router>
   );
 };
