54f5a2544d09d062edd7a596b028d5764def097b
Achyut katiyar <achyutkatiyar21@gmail.com>
2025-10-20 09:42:31 -0400

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC8gGYhg_-1TSBIlPJCxyCWcPWBBRs0EMU",
-  authDomain: "neunotes-21.firebaseapp.com",
-  projectId: "neunotes-21",
-  storageBucket: "neunotes-21.firebasestorage.app",
-  messagingSenderId: "1012336637127",
-  appId: "1:1012336637127:web:d08541c4fab1404e27bae2"
 };
 
 // Initialize Firebase app and auth
